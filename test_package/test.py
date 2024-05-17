c22ad38641debf3141c4ffad0a60948ff707e5ef
NoodleSushi <ytmikesstuffs@gmail.com>
2024-05-17 09:12:45 +0800

    asdf

--- test_package/test.py
+++ test_package/test.py
@@ -1,7 +1,11 @@
+import os
+script_dir = os.path.dirname(__file__)
+
 def greet():
     print("Hello, world!")
 
 def kek():
     # get text file from ./data/text.txt
-    with open("data/text.txt") as f:
+    text_file = os.path.join(script_dir, "data/text.txt")
+    with open(text_file) as f:
         print(f.read())
